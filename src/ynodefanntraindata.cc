b412bc2878cd3a825bb943e5f473d0d16fcd422d
cocoon-yang <mistyang@126.com>
2017-04-27 11:40:20 +0800

    Add files via upload

--- src/ynodefanntraindata.cc
+++ src/ynodefanntraindata.cc
@@ -1,8 +1,8 @@
 /*
  * ynodefanntraindata.cpp
  *
- *  Created on: April, 2017
- *      Author: Chunfeng Yang
+ *  Created on: 2017Äê4ÔÂ22ÈÕ
+ *      Author: GW00121076
  */
 
 #include "ynodefanntraindata.h"
@@ -47,6 +47,7 @@ void YNodeFANNTrainData::Init(Local<Object> exports) {
 	NODE_SET_PROTOTYPE_METHOD(tpl, "num_output_train_data", num_output_train_data);
 	NODE_SET_PROTOTYPE_METHOD(tpl, "length_train_data", length_train_data);
 
+	NODE_SET_PROTOTYPE_METHOD(tpl, "set_train_data", set_train_data);
 	NODE_SET_PROTOTYPE_METHOD(tpl, "read_train_from_file", read_train_from_file);
 
 	NODE_SET_PROTOTYPE_METHOD(tpl, "scale_train_data", scale_train_data);
@@ -58,8 +59,13 @@ void YNodeFANNTrainData::Init(Local<Object> exports) {
 	NODE_SET_PROTOTYPE_METHOD(tpl, "destroy", destroy);
 	NODE_SET_PROTOTYPE_METHOD(tpl, "save", save);
 
+	//NODE_SET_PROTOTYPE_METHOD(tpl, "length_train_data", length_train_data);
+
+
+
 	constructor.Reset(isolate, tpl->GetFunction());
 	exports->Set(String::NewFromUtf8(isolate, "YNodeFANNTrainData"), tpl->GetFunction());
+
 }
 
 
@@ -84,6 +90,172 @@ void YNodeFANNTrainData::New(const FunctionCallbackInfo<Value>& args) {
 	return;
 }
 
+
+
+//
+// void set_train_data(unsigned int num_data,
+//    unsigned int num_input, fann_type **input,
+//    unsigned int num_output, fann_type **output)
+//
+void YNodeFANNTrainData::set_train_data( const v8::FunctionCallbackInfo<v8::Value>& args)
+{
+	Isolate* isolate = args.GetIsolate();
+	YNodeFANNTrainData* obj = ObjectWrap::Unwrap<YNodeFANNTrainData>(args.Holder());
+
+	if (args.Length() < 1)
+	{
+		isolate->ThrowException(
+				v8::Exception::TypeError(
+						String::NewFromUtf8(isolate,
+								"Wrong arguments number")));
+		return;
+	}
+	// checking arguments
+	if (args[0]->IsUndefined())
+	{
+		isolate->ThrowException(
+				v8::Exception::TypeError(
+						String::NewFromUtf8(isolate,
+								"one argument is an undefined value.")));
+		return;
+	}
+
+	v8::Handle<v8::Array> dataArray;
+	if (args[0]->IsArray())
+	{
+		dataArray = v8::Handle<v8::Array>::Cast(args[0]);
+	}else	{
+		isolate->ThrowException(
+				v8::Exception::TypeError(
+						String::NewFromUtf8(isolate,
+								"argument is NOT an array.")));
+		return;
+	}
+
+	unsigned int num_data = dataArray->Length();
+	unsigned int num_input;
+	unsigned int num_output;
+
+	v8::Handle<v8::Value> tmp_arrayItem = dataArray->Get(
+					v8::Int32::New(isolate, 0));
+	v8::Handle<v8::Array> tmp_itemArray = v8::Handle<v8::Array>::Cast(tmp_arrayItem);
+	v8::Handle<v8::Value> tmp_inputItem = tmp_itemArray->Get(
+					v8::Int32::New(isolate, 0));
+	v8::Handle<v8::Value> tmp_outputItem = tmp_itemArray->Get(
+							v8::Int32::New(isolate, 1));
+	num_input = v8::Handle<v8::Array>::Cast(tmp_inputItem)->Length();
+	num_output = v8::Handle<v8::Array>::Cast(tmp_outputItem)->Length();
+
+	struct fann_train_data *data =
+	                (struct fann_train_data *)malloc(sizeof(struct fann_train_data));
+	if( NULL == data )
+	{
+		free(data);
+		data = NULL;
+		isolate->ThrowException(
+				v8::Exception::TypeError(
+						String::NewFromUtf8(isolate,
+								"allocating memory failed.")));
+		return;
+	}
+
+    data->num_data = num_data;
+    data->num_input = num_input;
+    data->num_output = num_output;
+	data->input  = NULL;
+	data->output  = NULL;
+
+	fann_type *data_input = (fann_type *)calloc(num_input*num_data, sizeof(fann_type));
+	fann_type *data_output = (fann_type *)calloc(num_output*num_data, sizeof(fann_type));
+
+	if( ( NULL == data_input ) || ( NULL == data_output )  )
+	{
+		free(data);
+		data = NULL;
+		free(data_input);
+		data_input = NULL;
+		free(data_output);
+		data_output = NULL;
+
+		isolate->ThrowException(
+				v8::Exception::TypeError(
+						String::NewFromUtf8(isolate,
+								"allocating memory failed.")));
+		return;
+	}
+
+	data->input = &data_input;
+	data->output = &data_output;
+
+	for (int i = 0; i < num_data; i++)
+	{
+		v8::Handle<v8::Value> arrayItem = dataArray->Get(
+				v8::Int32::New(isolate, i));
+
+		if ( !arrayItem->IsArray())
+		{
+			free(data);
+			data = NULL;
+			free(data_input);
+			data_input = NULL;
+			free(data_output);
+			data_output = NULL;
+			isolate->ThrowException(
+					v8::Exception::TypeError(
+							String::NewFromUtf8(isolate,
+									"argument is NOT an array.")));
+			return;
+		}
+
+		v8::Handle<v8::Array> itemArray = v8::Handle<v8::Array>::Cast(arrayItem);
+		v8::Handle<v8::Value> inputItem = itemArray->Get(
+						v8::Int32::New(isolate, 0));
+		v8::Handle<v8::Value> outputItem = itemArray->Get(
+								v8::Int32::New(isolate, 1));
+
+		if ( ( !inputItem->IsArray() ) || ( !outputItem->IsArray()  )  )
+		{
+			free(data);
+			data = NULL;
+			free(data_input);
+			data_input = NULL;
+			free(data_output);
+			data_output = NULL;
+			isolate->ThrowException(
+					v8::Exception::TypeError(
+							String::NewFromUtf8(isolate,
+									"argument is NOT an array.")));
+			return;
+		}
+
+		v8::Handle<v8::Array> inputArray = v8::Handle<v8::Array>::Cast(inputItem);
+		v8::Handle<v8::Array> outputArray = v8::Handle<v8::Array>::Cast(outputItem);
+
+		for (int j = 0; j < num_input; j++)
+		{
+			v8::Handle<v8::Value> theItem = inputArray->Get(
+							v8::Int32::New(isolate, j));
+			data_input[j] = theItem->NumberValue();
+
+		}
+		data_input += num_input;
+
+		for (int j = 0; j < num_output; j++)
+		{
+			v8::Handle<v8::Value> theItem = outputArray->Get(
+							v8::Int32::New(isolate, j));
+			data_output[j] = theItem->NumberValue();
+		}
+		data_output += num_output;
+	}
+    if (obj->train_data != NULL)
+    {
+        fann_destroy_train(obj->train_data);
+        obj->train_data = NULL;
+    }
+	obj->train_data = data;
+	return;
+}
 
 void YNodeFANNTrainData::read_train_from_file( const v8::FunctionCallbackInfo<v8::Value>& args)
 {
